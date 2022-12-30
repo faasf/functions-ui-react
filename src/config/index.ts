f531643cdee19cc6a937c48fd5d0c2bdcdd1184e
Julije Kostov <julijekos@gmail.com>
2022-12-30 12:30:36 +0100

    Fix config

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,7 @@ export const config = {
         clientId: process.env.REACT_APP_AUTH_CLIENT_ID || 'admin-ui',
         redirectUri: process.env.REACT_APP_AUTH_REDIRECT_URI || 'http://app.functions.local/authentication/callback',
         scopes: process.env.REACT_APP_AUTH_SCOPES || 'openid profile functions',
-        authority: process.env.REACT_APP_AUTH_AUTHORITY || 'http://keycloak.local/auth/realms/faas',
+        authority: process.env.REACT_APP_AUTH_AUTHORITY || 'http://keycloak.local/realms/faas',
     },
     functionServiceUrl: process.env.REACT_APP_FUNCTIONS_SERVICE_URL || 'http://functions.local/',
     logsServiceUrl: process.env.REACT_APP_LOGS_SERVICE_URL || 'http://functions.local/as',
